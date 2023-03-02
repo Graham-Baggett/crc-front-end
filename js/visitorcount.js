e1513d44f5e6b955e6d5778417553040f06d2283
Graham Baggett <graham.l.baggett@census.gov>
2023-03-02 15:32:28 -0500

    Updated API Gateway URL
    The API Gateway has been re-created, which caused the URL to change.
    I have updated the URL in visitorcount.js to reflect this

--- js/visitorcount.js
+++ js/visitorcount.js
@@ -1,7 +1,8 @@
-fetch('https://thmc67kpn2.execute-api.us-east-1.amazonaws.com/prod/put')
-    .then(() => fetch('https://thmc67kpn2.execute-api.us-east-1.amazonaws.com/prod/get'))
-    .then(response => response.json())
-    .then((data) => {
-                        document.getElementById('visitorcount').innerText = data
-                    }
-     )
+fetch("https://ufh573r6ol.execute-api.us-east-1.amazonaws.com/Prod/put")
+  .then(() =>
+    fetch("https://ufh573r6ol.execute-api.us-east-1.amazonaws.com/Prod/get")
+  )
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("visitorcount").innerText = data;
+  });
