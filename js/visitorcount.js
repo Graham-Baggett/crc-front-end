d7d4ad26ccaa705036224b9603b495a128095efb
Graham Baggett <graham.l.baggett@census.gov>
2023-03-07 09:59:35 -0500

    updated visitorcount based on ChatGPT recommendations
    visitorcount.js now has better error handling.
    It uses const for visitorCount to avoid an implicit global variable.
    Finally, it uses async so that it can use await

--- js/visitorcount.js
+++ js/visitorcount.js
@@ -1,8 +1,24 @@
-fetch("https://ufh573r6ol.execute-api.us-east-1.amazonaws.com/Prod/put")
-  .then(() =>
-    fetch("https://ufh573r6ol.execute-api.us-east-1.amazonaws.com/Prod/get")
-  )
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("visitorcount").innerText = data;
-  });
+(async function () {
+  try {
+    const putResponse = await fetch(
+      "https://ufh573r6ol.execute-api.us-east-1.amazonaws.com/Prod/put"
+    );
+    if (!putResponse.ok) {
+      throw new Error("Failed to update visitor count");
+    }
+
+    const getResponse = await fetch(
+      "https://ufh573r6ol.execute-api.us-east-1.amazonaws.com/Prod/get"
+    );
+    if (!getResponse.ok) {
+      throw new Error("Failed to get visitor count");
+    }
+
+    const data = await getResponse.json();
+    const visitorCount = document.getElementById("visitorcount");
+    visitorCount.innerText = data;
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately
+  }
+})();
