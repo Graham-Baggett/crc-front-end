e1ea32e8054f0e8c8ece37133afadf54e8a0770b
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-21 15:44:29 -0400

    Aligned = signs to conform to terraform fmt standards

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "domain_name" {
-  type = string
-  default = "grahambaggett.com"
+  type        = string
+  default     = "grahambaggett.com"
   description = "Custom domain name"
 }
 
 variable "bucket_name" {
-  type = string
-  default = "gb-cloud-resume"
+  type        = string
+  default     = "gb-cloud-resume"
   description = "Custom domain name"
 }
