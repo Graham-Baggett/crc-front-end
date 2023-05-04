9b86e68b6ad1fde88576d92ff6a2d24c48620975
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-04 13:42:44 -0400

    Added reference to bucket_access_type in create_bucket resource
    Now, the bucket's access policy will be driven by the variables.bucket_access_type value

--- terraform/website_bucket.tf
+++ terraform/website_bucket.tf
@@ -3,6 +3,7 @@ resource "oci_objectstorage_bucket" "create_bucket" {
     compartment_id = var.compartment_ocid
     name = var.bucket_name
     namespace = var.bucket_namespace
+    access_type = var.bucket_access_type
 }
 
 output "new_bucket" {
