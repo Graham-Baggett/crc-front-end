ec896db892475e97037cd6feb9cccf20aaf761da
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-04 17:11:08 -0400

    Added oci_dns_rrset to website_domain.tf

--- terraform/website_domain.tf
+++ terraform/website_domain.tf
@@ -4,3 +4,10 @@ resource "oci_dns_zone" "hosted_zone" {
     name = var.zone_name
     zone_type = var.zone_type
 }
+
+resource "oci_dns_rrset" "zone_rrset" {
+    #Required
+    domain = var.rrset_domain
+    rtype = var.rrset_rtype
+    zone_name_or_id = oci_dns_zone.test_zone.id
+}
