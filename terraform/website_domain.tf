7c6652e1783ce83c95d87a73fc5da69151c49f4c
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-05 09:41:19 -0400

    Update website_domain.tf
    Updated rtype variable setting and fixed the name of the oci_dns_zone reference

--- terraform/website_domain.tf
+++ terraform/website_domain.tf
@@ -8,6 +8,6 @@ resource "oci_dns_zone" "hosted_zone" {
 resource "oci_dns_rrset" "zone_rrset" {
     #Required
     domain = var.rrset_domain
-    rtype = var.rrset_rtype
-    zone_name_or_id = oci_dns_zone.test_zone.id
+    rtype = var.rrset_a_rtype
+    zone_name_or_id = oci_dns_zone.hosted_zone.id
 }
