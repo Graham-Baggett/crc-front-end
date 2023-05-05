b3d1278812d347ead82a8b8c348846594627357c
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-05 13:56:11 -0400

    Added http_ prefix to most redirect variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -62,25 +62,25 @@ variable "rrset_items_ttl" {
   description = "Domain where Cloud Resume Challenge website resides"
 }
 
-variable "redirect_target_host" {
+variable "http_redirect_target_host" {
   type        = string
   default     = "objectstorage.us-ashburn-1.oraclecloud.com"
   description = "Base URL where Cloud Resume Challenge website resides"
 }
 
-variable "redirect_target_path" {
+variable "http_redirect_target_path" {
   type        = string
   default     = "/n/idixwybnkjfi/b/gb-cloud-resume/o/html/index.html"
   description = "Path where Cloud Resume Challenge index site resides"
 }
 
-variable "redirect_target_protocol" {
+variable "http_redirect_target_protocol" {
   type        = string
   default     = "https"
   description = "TCP/IP Protocol for the Cloud Resume Challenge website"
 }
 
-variable "redirect_target_port" {
+variable "http_redirect_target_port" {
   type        = number
   default     = 443
   description = "TCP/IP Protocol Port for the Cloud Resume Challenge website"
