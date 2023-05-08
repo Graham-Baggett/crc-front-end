1406afcc93ff46869d469e63a0be42dc6971511f
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-08 11:52:49 -0400

    Added public-security-list-name output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,11 @@
-output "private-security-list-name" {
-  value = oci_core_security_list.private-security-list.display_name
-}
-
-
 output "gb-cloud-resume_bucket" {
     value = oci_objectstorage_bucket.create_bucket
+}
+
+output "private-security-list-name" {
+  value = oci_core_security_list.private_security_list.display_name
+}
+
+output "public-security-list-name" {
+  value = oci_core_security_list.public_security_list.display_name
 }
