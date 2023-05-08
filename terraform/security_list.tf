0ba8b9eac8edb1daf96792eb568c8868884e07ca
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-08 08:45:55 -0400

    Added ingress rules to security_list.tf

--- terraform/security_list.tf
+++ terraform/security_list.tf
@@ -2,4 +2,11 @@ resource "oci_core_security_list" "public_security_list" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_vcn.main_vcn.id
   display_name   = "cloud-resume-challenge-public-security-list"
+  
+  ingress_security_rules {
+    description = "Main VCN Ingress Rules"
+    protocol    = var.ingress["protocol"]
+    source      = var.ingress["source"]
+    stateless   = var.ingress["stateless"]
+  }
 }
