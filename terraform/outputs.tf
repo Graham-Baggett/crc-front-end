d838f308c51a3d3bc9a90bfa7051242d5ec9f2c2
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-08 09:48:57 -0400

    Moved gb-cloud-resume bucket output to outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "private-security-list-name" {
   value = oci_core_security_list.private-security-list.display_name
 }
+
+
+output "gb-cloud-resume_bucket" {
+    value = oci_objectstorage_bucket.create_bucket
+}
