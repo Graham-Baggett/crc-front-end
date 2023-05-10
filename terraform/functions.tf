4107b4edc39b6e9d20f49396a0e0b30da3a7018b
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-10 13:45:19 -0400

    container repo's display name references a var [skip ci]

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -2,7 +2,7 @@
 resource "oci_artifacts_container_repository" "container_repository_for_function" {
     # note: repository = store for all images versions of a specific container image - so it included the function name
     compartment_id = var.ocid_compartment_ocid
-    display_name = "${local.ocir_repo_name}/${var.function_name}"
+    display_name = var.container_registry_name
     is_immutable = false
     is_public = false
 }
