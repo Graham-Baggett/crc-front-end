316051bb174052a9c94d451809c8883a30ac3f59
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-10 13:49:42 -0400

    Fixed reference to compartment_ocid variable

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -1,7 +1,7 @@
 ### Repository in the Container Image Registry for the container images underpinning the function 
 resource "oci_artifacts_container_repository" "container_repository_for_function" {
     # note: repository = store for all images versions of a specific container image - so it included the function name
-    compartment_id = var.ocid_compartment_ocid
+    compartment_id = var.compartment_ocid
     display_name = var.container_registry_name
     is_immutable = false
     is_public = false
