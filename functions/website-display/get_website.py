d25ab4dc88722dbbd372e9a5dd2a4f60817c6c06
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-11 16:58:00 -0400

    Create get_website.py [skip ci]

---
+++ functions/website-display/get_website.py
@@ -0,0 +1,34 @@
+def handler(ctx, data: io.BytesIO=None):
+    if None == ctx.RequestURL():
+        return "Function loaded properly but not invoked via an HTTP request."
+    signer = oci.auth.signers.get_resource_principals_signer()
+    logging.getLogger().info("URI: " + ctx.RequestURL() )
+    config = {
+        # update with your tenancy's OCID
+        "tenancy": "ocid1.tenancy.oc1..aaaaaaaao6t6gpu73hsg3g2kalw3vvg3sicohgtzdv5emcdrblxkzvnm4fza",
+        # replace with the region you are using
+        "region": "us-ashburn-1"
+    }
+    try:
+        object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
+        namespace = object_storage.get_namespace().data
+        # update with your bucket name
+        bucket_name = "demo"
+        file_object_name = ctx.RequestURL()
+        if file_object_name.endswith("/"):
+            logging.getLogger().info("Adding index.html to request URL " + file_object_name)
+            file_object_name += "index.html"
+
+        # strip off the first character of the URI (i.e. the /)
+        file_object_name = file_object_name[1:]
+
+        obj = object_storage.get_object(namespace, bucket_name, file_object_name)
+        return response.Response(
+            ctx, response_data=obj.data.content,
+            headers={"Content-Type": obj.headers['Content-type']}
+        )
+    except (Exception) as e:
+        return response.Response(
+            ctx, response_data="500 Server error",
+            headers={"Content-Type": "text/plain"}
+            )
