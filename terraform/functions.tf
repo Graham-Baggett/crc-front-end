954db2bfc4b2e5f98ed69ba6a4bfe24e27ba6665
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-12 15:59:05 -0400

    Added oci_functions_function to functions.tf [skip ci]

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -1,4 +1,4 @@
-resource "oci_functions_application" "cloud_resume_application_application" {
+resource "oci_functions_application" "cloud_resume_challenge_application" {
   compartment_id = var.compartment_ocid
   display_name   = "${var.application_name}"
   subnet_ids     = [oci_core_subnet.public_subnet.id]
@@ -11,4 +11,11 @@ resource "oci_artifacts_container_repository" "container_repository_for_function
     display_name = "${var.container_registry_name}/${var.website_display_function_name}"
     is_immutable = false
     is_public = false
+}
+
+resource "oci_functions_function" "website_function" {
+  application_id = "${local.application_id}"
+  display_name   = "${var.function_name}"
+  image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${local.ocir_repo_name}/${var.function_name}:0.0.1"
+  memory_in_mbs  = "128"
 }
