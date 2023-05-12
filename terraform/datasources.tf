e0e8186a0e9c46135aee54de6d269528b810e576
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-12 16:45:07 -0400

    add data sources to datasources.tf [skip ci]

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -6,3 +6,43 @@ data "oci_functions_applications" "crc_function_applications" {
 locals {
   application_id = data.oci_functions_applications.crc_function_applications.applications[0].id
 }
+
+# Gets home and current regions
+data "oci_identity_tenancy" "tenant_details" {
+  tenancy_id = var.tenancy_ocid
+}
+
+data "oci_identity_regions" "home_region" {
+  filter {
+    name   = "key"
+    values = [data.oci_identity_tenancy.tenant_details.home_region_key]
+  }
+}
+
+
+data "oci_identity_tenancy" "oci_tenancy" {
+  tenancy_id = var.tenancy_ocid
+}
+
+# OCI Services
+## Available Services
+data "oci_core_services" "all_services" {
+  filter {
+    name   = "name"
+    values = ["All .* Services In Oracle Services Network"]
+    regex  = true
+  }
+}
+
+data "oci_identity_regions" "oci_regions" {
+
+  filter {
+    name   = "name"
+    values = [var.region]
+  }
+
+}
+
+data "oci_objectstorage_namespace" "os_namespace" {
+  compartment_id = var.tenancy_ocid
+}
