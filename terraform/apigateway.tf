6402e971b0ec5d590d65ffc4f319a814dcf13ea8
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-12 11:28:30 -0400

    Add oci_identity_policy resource
    This should let the API Gateway access the Functions

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -6,3 +6,12 @@ resource "oci_apigateway_gateway" "website_api_gateway" {
     prevent_destroy = false
   }
 }
+
+resource "oci_identity_policy" "api_gateway_fnpolicy" {
+  compartment_id = var.compartment_ocid
+  description    = "APIGW policy for compartment to access FN"
+  name           = "apigateway_fn_policies"
+  statements = [
+    "ALLOW any-user to use functions-family in compartment id ${var.compartment_ocid} where ALL {request.principal.type= 'ApiGateway', request.resource.compartment.id = '${var.compartment_ocid}'}"
+  ]
+}
