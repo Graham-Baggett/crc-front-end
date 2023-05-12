954ef71cfec1670c910f2ec12fcd55c52976017f
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-12 09:23:26 -0400

    Added oci_functions_applications resource [skip ci]

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -1,3 +1,8 @@
+resource "oci_functions_applications" "cloud_resume_application_application" {
+  compartment_id = var.compartment_ocid
+  display_name   = "${var.application_name}"
+}
+
 ### Repository in the Container Image Registry for the container images underpinning the function 
 resource "oci_artifacts_container_repository" "container_repository_for_function" {
     # note: repository = store for all images versions of a specific container image - so it included the function name
