33e02c3ce0bc0da0fbdee3b461b1cbc482e426d2
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-15 17:01:01 -0400

    Add testing to functions.tf

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -19,3 +19,27 @@ resource "oci_functions_function" "website_function" {
   image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${local.ocir_repo_name}/${var.function_name}:0.0.1"
   memory_in_mbs  = "128"
 }
+
+### wait a little while before the function is ready to be invoked to avoid the below error
+## Error: 404-NotAuthorizedOrNotFound 
+## Error Message: Authorization failed or requested resource not found 
+##│Suggestion: Either the resource has been deleted or service Functions Invoke Function need policy to access this resource. 
+resource "time_sleep" "wait_for_function_to_be_ready" {
+  depends_on = [oci_functions_function.website_function]
+  create_duration = "10s"
+}
+
+resource "oci_functions_invoke_function" "test_invoke_website_function" {
+  depends_on     = [time_sleep.wait_for_function_to_be_ready]
+    #Required
+    function_id = oci_functions_function.website_function.id
+
+    #Optional
+    fn_intent = "httprequest"
+    fn_invoke_type = "sync" 
+    base64_encode_content = false
+}
+
+output "function_response" {
+  value = oci_functions_invoke_function.test_invoke_website_function.content
+}
