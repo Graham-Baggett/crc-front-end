ff749239ac137088d1d28e989829ca3be6215b96
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-18 17:14:06 -0400

    Added log group for apigateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -50,3 +50,9 @@ resource "oci_identity_policy" "api_gateway_fnpolicy" {
     "ALLOW any-user to use functions-family in compartment id ${var.compartment_ocid} where ALL {request.principal.type= 'ApiGateway', request.resource.compartment.id = '${var.compartment_ocid}'}"
   ]
 }
+
+resource "oci_logging_log_group" "api_log_group" {
+    #Required
+    compartment_id = var.compartment_ocid
+    display_name = "website-api-gateway-log-group"
+}
