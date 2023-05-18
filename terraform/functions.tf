1e489ccb72371fb9bdd484f397615c6c890f9989
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-05-18 11:00:20 -0400

    Changed website_display_function_name to function_name [skip ci]

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -8,7 +8,7 @@ resource "oci_functions_application" "cloud_resume_challenge_application" {
 resource "oci_artifacts_container_repository" "container_repository_for_function" {
     # note: repository = store for all images versions of a specific container image - so it included the function name
     compartment_id = var.compartment_ocid
-    display_name = "${var.container_registry_name}/${var.website_display_function_name}"
+    display_name = "${var.container_registry_name}/${var.function_name}"
     is_immutable = false
     is_public = false
 }
@@ -16,7 +16,7 @@ resource "oci_artifacts_container_repository" "container_repository_for_function
 resource "oci_functions_function" "website_function" {
   application_id = "${local.application_id}"
   display_name   = "${var.function_name}"
-  image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${local.ocir_repo_name}/${var.website_display_function_name}:0.0.1"
+  image          = "${local.ocir_docker_repository}/${local.ocir_namespace}/${local.ocir_repo_name}/${var.function_name}:0.0.1"
   memory_in_mbs  = "128"
 }
 
@@ -49,14 +49,3 @@ resource "oci_logging_log" "crc_log" {
 
     retention_duration = var.log_retention_duration
 }
-
-# module "logging_function" {
-#   source  = "oracle-terraform-modules/logging/oci//modules/function"
-#   version = "0.4.0"
-#   # insert the 4 required variables here
-#   compartment_id = var.compartment_ocid
-#   loggroup = oci_logging_log_group.funcloggroup
-#   logdefinition = local.funclogdef
-  
-#   log_retention_duration = var.log_retention_duration
-# }
