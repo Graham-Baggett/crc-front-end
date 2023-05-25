11f1aaca928fb56fc4202c8b627ae57af00358a0
Graham Baggett <graham.l.baggett@census.gov>
2023-05-25 11:32:40 -0400

    Config var removed from func.py
    This removes the hardcoded tenancy_ocid and region values from the file

--- functions/website-display/func.py
+++ functions/website-display/func.py
@@ -3,17 +3,13 @@ import oci
 import logging
 from fdk import response
 
-def handler(ctx, data: io.BytesIO=None):
+
+def handler(ctx, data: io.BytesIO = None):
     if None == ctx.RequestURL():
         return "Function loaded properly but not invoked via an HTTP request."
     signer = oci.auth.signers.get_resource_principals_signer()
-    logging.getLogger().info("URI: " + ctx.RequestURL() )
-    config = {
-        # update with your tenancy's OCID
-        "tenancy": "ocid1.tenancy.oc1..aaaaaaaao6t6gpu73hsg3g2kalw3vvg3sicohgtzdv5emcdrblxkzvnm4fza",
-        # replace with the region you are using
-        "region": "us-ashburn-1"
-    }
+    logging.getLogger().info("URI: " + ctx.RequestURL())
+
     try:
         # object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
         object_storage = oci.object_storage.ObjectStorageClient({}, signer=signer)
@@ -22,7 +18,9 @@ def handler(ctx, data: io.BytesIO=None):
         bucket_name = "gb-cloud-resume"
         file_object_name = ctx.RequestURL()
         if file_object_name.endswith("/"):
-            logging.getLogger().info("Adding html/index.html to request URL " + file_object_name)
+            logging.getLogger().info(
+                "Adding html/index.html to request URL " + file_object_name
+            )
             file_object_name += "html/index.html"
 
         # strip off the first character of the URI (i.e. the /)
@@ -30,11 +28,11 @@ def handler(ctx, data: io.BytesIO=None):
 
         obj = object_storage.get_object(namespace, bucket_name, file_object_name)
         return response.Response(
-            ctx, response_data=obj.data.content,
-            headers={"Content-Type": obj.headers['Content-type']}
+            ctx,
+            response_data=obj.data.content,
+            headers={"Content-Type": obj.headers["Content-type"]},
         )
-    except (Exception) as e:
+    except Exception as e:
         return response.Response(
-            ctx, response_data=str(e),
-            headers={"Content-Type": "text/plain"}
-            )
+            ctx, response_data=str(e), headers={"Content-Type": "text/plain"}
+        )
