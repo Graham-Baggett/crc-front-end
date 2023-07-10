bf9195a086435d9876c8de90f3985b7259bb8fef
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-07-10 16:35:39 -0400

    Added rule to security_list.tf

--- terraform/security_list.tf
+++ terraform/security_list.tf
@@ -33,6 +33,18 @@ resource "oci_core_security_list" "public_security_list" {
           max = 443
       }
   }
+
+  ingress_security_rules { 
+      stateless = var.ingress_variables["stateless"]
+      source = "0.0.0.0/0"
+      source_type = var.ingress_variables["source_type"]
+      # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+      protocol = "6"
+      tcp_options { 
+          min = 5000
+          max = 5000
+      }
+  }
   
   ingress_security_rules { 
       stateless = var.ingress_variables["stateless"]
